65af7b02dc5645907d2bd60fb67fb37de64f96fe
daidh25 <dinhhuydai1234@gmail.com>
2024-04-06 09:40:10 +0700

    btvnbuoi5

---
+++ buoi5-bai1/Program.cs
@@ -0,0 +1,138 @@
+﻿using System.Globalization;
+using System.Text;
+
+internal class Program
+{
+    public struct NhanVien
+    {
+        public string MaNV { get; set; }
+        public string HoDem { get; set; }
+        public string Ten {  get; set; }
+        public DateTime NgayThangNamSinh { get; set; }
+        public DateTime NgayVaoLamViec { get; set; }
+        public NhanVien(string maNV, string hoDem,string ten, DateTime ngayThangNamSinh, DateTime ngayVaoLamViec)
+        {
+            MaNV = maNV;
+            HoDem = hoDem;
+            Ten = ten;
+            NgayThangNamSinh = ngayThangNamSinh;
+            NgayVaoLamViec = ngayVaoLamViec;
+        }
+        public int SoNamLamViec()
+        {
+            DateTime ngayHienTai = DateTime.Now;
+            int soNam = ngayHienTai.Year - NgayVaoLamViec.Year;
+            if (ngayHienTai.Month < NgayVaoLamViec.Month || (ngayHienTai.Month == NgayVaoLamViec.Month && ngayHienTai.Day < NgayVaoLamViec.Day))
+                soNam--;
+            return soNam;
+        }
+
+    }
+    
+    public static void NhapDS(List<NhanVien> ds)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("Nhập số lượng nhân viên: ");
+        int n = int.Parse(Console.ReadLine());
+        while (n <= 0)
+        {
+            Console.WriteLine("Vui lòng nhập số nguyên dương !!!!");
+            n = int.Parse(Console.ReadLine());
+        }
+        for(int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Nhập nhân viên thứ: " + (i + 1));
+            Console.Write("Nhập mã nhân viên: ");
+            string MaNV = Console.ReadLine();
+            while (string.IsNullOrEmpty(MaNV))
+            {
+                Console.WriteLine("Mã nhân viên không được để trống. Vui lòng nhập lại !!!");
+                MaNV = Console.ReadLine();
+            }
+            Console.Write("Nhập họ đệm: ");
+            string HoDem = Console.ReadLine();
+            while (string.IsNullOrEmpty(HoDem))
+            {
+                Console.WriteLine("Họ đệm không được để trống. Vui lòng nhập lại !!!");
+            }
+            Console.Write("Nhập tên: ");
+            string Ten = Console.ReadLine();
+            while (string.IsNullOrEmpty(Ten))
+            {
+                Console.WriteLine("Tên không được để trống. Vui lòng nhập lại !!!");
+            }
+            Console.Write("Nhập ngày tháng năm sinh theo định dạng (dd/mm/yyyy):");
+            DateTime NgayThangNamSinh;
+            while (!DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out NgayThangNamSinh))
+            {
+                Console.Write("Ngày tháng năm sinh nhập vào không hợp lệ. Vui lòng nhập lại theo định dạng dd/MM/yyyy: ");
+            }
+            Console.Write("Nhập ngày vào làm: ");
+            DateTime NgayVaoLam;
+            while (!DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out NgayVaoLam))
+            {
+                Console.Write("Ngày vào làm nhập vào không hợp lệ. Vui lòng nhập lại theo định dạng dd/MM/yyyy: ");
+            }
+            NhanVien x = new NhanVien(MaNV, HoDem, Ten, NgayThangNamSinh, NgayVaoLam);
+            ds.Add(x);
+
+        }
+        
+
+    }
+    public static void HienThiDS(List<NhanVien> ds)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("\n\nHiện Thị Danh Sách:");
+        for(int i = 0; i < ds.Count; i++)
+        {
+            Console.WriteLine("Mã nhân viên: " + ds[i].MaNV);
+            Console.WriteLine("Họ đệm: " + ds[i].HoDem);
+            Console.WriteLine("Tên  nhân viên: " + ds[i].Ten);
+            Console.WriteLine("Ngày tháng năm sinh: " + ds[i].NgayThangNamSinh);
+            Console.WriteLine("Ngày vào làm: " + ds[i].NgayVaoLamViec);
+            Console.WriteLine("--------------------------------------------------");
+        }
+
+    }
+    public static void SapXepTheoNgaySinhGiam(List<NhanVien> ds)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("\n\nDanh sách nhân viên sắp xếp ngày sinh theo thứ tự giảm dần ");
+        ds.Sort((x, y) => y.NgayThangNamSinh.CompareTo(x.NgayThangNamSinh));
+    }
+    public static void KinhNghiemLamViec(List<NhanVien> ds)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("\n\nDanh Sách Các Nhân Viên Có Số Năm Làm Việc Lớn Hơn Hoặc Bằng 5:");
+        for (int i = 0; i < ds.Count; i++)
+        {
+            int soNamLamViec = ds[i].SoNamLamViec();
+            if (soNamLamViec >= 5)
+            {
+                Console.WriteLine("Mã nhân viên: " + ds[i].MaNV);
+                Console.WriteLine("Họ đệm: " + ds[i].HoDem);
+                Console.WriteLine("Tên  nhân viên: " + ds[i].Ten);
+                Console.WriteLine("Ngày tháng năm sinh: " + ds[i].NgayThangNamSinh);
+                Console.WriteLine("Ngày vào làm: " + ds[i].NgayVaoLamViec);
+                Console.WriteLine("Số năm làm việc: " + soNamLamViec);
+                Console.WriteLine("--------------------------------------------------");
+            }
+            else
+            {
+                Console.WriteLine("Không có nhân viên nào kinh nghiệm >=5.!!!");
+            }
+            
+        }
+    }
+    private static void Main(string[] args)
+    {
+        List<NhanVien> ds = new List<NhanVien>();
+        NhapDS(ds);
+        HienThiDS(ds);
+        SapXepTheoNgaySinhGiam(ds);
+        HienThiDS(ds);
+        KinhNghiemLamViec(ds);
+
+    }
+}
---
+++ buổi5-bài2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Globalization;
+using System.Text;
+
+internal class Program
+{
+    public struct Product
+    {
+        public string ProDuctName {  get; set; }
+        public double ProDuctPrice { get; set; }
+        public DateTime ExpiredDate { get; set; }
+        public Product(string productName, double productPrice,DateTime expiredDate)
+        {
+            ProDuctName = productName;
+            ProDuctPrice = productPrice;
+            ExpiredDate = expiredDate;
+        }
+        public bool IsExpired()
+        {
+            TimeSpan remainingTime = ExpiredDate - DateTime.Today;
+            return remainingTime.TotalDays < 180;
+        }
+    }
+    public static void NhapTTSP(List<Product> ds)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("Nhập số lượng sản phẩm: ");
+        int n = int.Parse(Console.ReadLine());
+        while (n <= 0)
+        {
+            Console.WriteLine("Vui lòng nhập số nguyên dương !!!");
+            n= int.Parse(Console.ReadLine());
+        }
+        for(int i = 0;i<n;i++)
+        {
+            Console.WriteLine("Nhập sản phẩm thứ: "+(i+1));
+            Console.Write("Nhập ProductName: ");
+            string ProDuctName = Console.ReadLine();
+            while(string.IsNullOrEmpty(ProDuctName))
+            {
+                Console.Write("Vui lòng nhập ProductName.Không được để trống ! ");
+                ProDuctName = Console.ReadLine();
+            }
+            Console.Write("Nhập ProductPrice: ");
+            double ProDuctPrice = Convert.ToDouble(Console.ReadLine());
+            while (ProDuctPrice < 0)
+            {
+                Console.WriteLine("Gía sản phẩm không âm.Vui lòng nhập lại !!");
+                ProDuctPrice = Convert.ToDouble(Console.ReadLine());
+            }
+            Console.Write("Nhập ExpiredDate theo dạng (dd/mm/yyyy): ");
+            DateTime ExpiredDate;
+            while (!DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out ExpiredDate))
+            {
+                Console.Write("Ngày sản phẩm hết hạn nhập vào không hợp lệ. Vui lòng nhập lại theo định dạng dd/MM/yyyy: ");
+            }
+            Console.WriteLine();
+            Product x = new Product( ProDuctName, ProDuctPrice, ExpiredDate);
+            ds.Add(x);
+        }
+    }
+    private static void Main(string[] args)
+    {
+        List<Product> ds = new List<Product>();
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("=====================Hiển Thị Thông Tin Sản Phẩm ====================");
+        NhapTTSP(ds);
+        Console.WriteLine("\nCác sản phẩm có ngày hết hạn trong vòng 30 ngày:");
+        for (int i = 0; i < ds.Count; i++)
+        {
+            if (ds[i].IsExpired())
+            {
+                Console.WriteLine($"Tên sản phẩm: {ds[i].ProDuctName}, Giá sản phẩm: {ds[i].ProDuctPrice}, Ngày hết hạn: {ds[i].ExpiredDate:dd/MM/yyyy}");
+            }
+        }
+
+       
+        Console.WriteLine("\nCác sản phẩm có tên sản phẩm dài hơn 10 ký tự:");
+        for (int i = 0; i < ds.Count; i++)
+        {
+            if (ds[i].ProDuctName.Length > 10)
+            {
+                Console.WriteLine($"Tên sản phẩm: {ds[i].ProDuctName}, \nGiá sản phẩm: {ds[i].ProDuctPrice}, \nNgày hết hạn: {ds[i].ExpiredDate:dd/MM/yyyy}");
+            }
+            else
+            {
+                Console.WriteLine("Không có sản phẩm nào kí tự dài hơn 10...");
+            }
+        }
+    }
+}
